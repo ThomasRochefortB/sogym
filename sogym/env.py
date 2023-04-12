ce51f65b1f23db8a1df1f15d9f72ac5ad5ecac6f
ThomasRochefortB <thomas.rochefort.beaudoin@gmail.com>
2023-04-12 09:46:51 -0400

    bugfix check_connec

--- sogym/env.py
+++ sogym/env.py
@@ -423,11 +423,10 @@ class sogym(gym.Env):
                 labels_load .append(labels[-1,int(self.conditions['load_position'][i]*self.nelx)])
 
         for load in labels_load:
-             for load in labels_load:
-                if load in label_support and load!=0:
-                    connec.append(True)
-                else:
-                    connec.append(False)
+            if load in label_support and load!=0:
+                connec.append(True)
+            else:
+                connec.append(False)
         # return True if connec is not empty and if all its elements are True
         return bool(connec) and all(connec)
     
