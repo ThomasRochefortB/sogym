e892de7226b9d2b25dbc3022fde17c0e8aad43a4
ThomasRochefortB <thomas.rochefort.beaudoin@gmail.com>
2024-04-22 16:59:49 -0400

    m

--- dreamer.py
+++ dreamer.py
@@ -1,3 +1,5 @@
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'       #Disactivate multiprocessing for numpy
 def main():
   from sogym.env_gym import sogym
   import warnings
@@ -12,7 +14,7 @@ def main():
   config = embodied.Config(dreamerv3.configs['defaults'])
   config = config.update(dreamerv3.configs['medium'])
   config = config.update({
-      'logdir': './logdir/run1',
+      'logdir': './logdir/run3',
       'run.train_ratio': 64,
       'run.log_every': 30,  # Seconds
       'batch_size': 16,
@@ -20,7 +22,7 @@ def main():
       'decoder.mlp_keys':  ['beta','volume','design_variables','n_steps_left'],
       'encoder.cnn_keys': ['image','structure_strain_energy'],
       'decoder.cnn_keys': ['image','structure_strain_energy'],
-       'jax.platform': 'cpu',
+       'jax.platform': 'gpu',
       # 'encoder.resize':'stride3',
       # 'decoder.resize':'stride3',
 
@@ -38,19 +40,26 @@ def main():
 
 
   from embodied.envs import from_gym
-  env = sogym(mode='train',observation_type='dense',vol_constraint_type = 'hard',resolution=50,img_format = 'HWC',check_connectivity=True) # Replace this with your Gym env.
+  env = sogym(mode='train',observation_type='topopt_game',vol_constraint_type = 'hard',resolution=50,img_format = 'HWC',check_connectivity=True) # Replace this with your Gym env.
   # env = StepAPICompatibility(env[0])
   env = from_gym.FromGym(env)
 
   env = dreamerv3.wrap_env(env, config)
   env = embodied.BatchEnv([env], parallel=False)
 
+  eval_env = sogym(mode='test',observation_type='topopt_game',vol_constraint_type = 'hard',resolution=50,img_format = 'HWC',check_connectivity=True) # Replace this with your Gym env.
+  # env = StepAPICompatibility(env[0])
+  eval_env = from_gym.FromGym(eval_env)
+  eval_env = dreamerv3.wrap_env(eval_env, config)
+  eval_env = embodied.BatchEnv([eval_env], parallel=False)
+
   agent = dreamerv3.Agent(env.obs_space, env.act_space, step, config)
   replay = embodied.replay.Uniform(config.batch_length, config.replay_size, logdir / 'replay')
+  eval_replay = embodied.replay.Uniform(config.batch_length, config.replay_size, logdir / 'evalreplay')
   args = embodied.Config(**config.run, logdir=config.logdir, batch_steps=config.batch_size * config.batch_length)
-  embodied.run.train(agent, env, replay, logger, args)
-
-
+  # embodied.run.train(agent, env, replay, logger, args)
+  embodied.run.train_eval(agent,env, eval_env, replay, eval_replay, logger, args)
+  
 
 if __name__ == '__main__':
   main()
