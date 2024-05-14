d90ff96cc43637a6e63c3089a24eb99d4d11c38c
ThomasRochefortB <thomas.rochefort.beaudoin@gmail.com>
2024-05-14 11:28:59 -0400

    m

--- gen_samples.py
+++ gen_samples.py
@@ -2,4 +2,4 @@ import os
 os.environ['OPENBLAS_NUM_THREADS'] = '1'       #Disactivate multiprocessing for numpy
 from sogym.expert_generation import generate_dataset
 
-generate_dataset(dataset_folder = "./dataset/topologies/holodeck_may12",num_threads = 32, num_samples=10000)
+generate_dataset(dataset_folder = "./dataset/topologies/cortex_may13",num_threads = 24, num_samples=10000)
