ec39ae06afc339c3192b3e3e38476a18c27ff47c
ThomasRochefortB <thomas.rochefort.beaudoin@gmail.com>
2024-05-16 13:35:15 -0400

    fixed rl_training script

--- rl_training.py
+++ rl_training.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from stable_baselines3.common.noise import NormalActionNoise
 import stable_baselines3
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv
+from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv, VecCheckNan
 from stable_baselines3 import PPO, A2C, SAC, TD3
 from stable_baselines3.common.callbacks import EvalCallback, CallbackList, CheckpointCallback, StopTrainingOnNoModelImprovement
 
@@ -37,8 +37,15 @@ parser = argparse.ArgumentParser(description='Pretrain RL agent with expert data
 
 parser.add_argument('--observation-type', type=str, default="topopt_game",
                     help='Type of observation used in the environment.')
-parser.add_argument('--algorithm-name', type=str, default="SAC",
+
+parser.add_argument('--vol-constraint-type', type=str, default="hard")
+
+parser.add_argument('--use-std-strain', action='store_true', help='Use standard strain in the observation.')
+
+
+parser.add_argument('--algorithm-name', type=str, default="PPO",
                     help='Algorithm to use for training (SAC, PPO, TD3).')
+
 parser.add_argument('--algorithm-config-file', type=str, default="algorithms.yaml",
                     help='Path to the algorithm configuration file.')
 
@@ -50,6 +57,7 @@ parser.add_argument('--training-phase', type=str, default='naive')
 
 parser.add_argument('--replay-buffer-path', type=str, default='')
 
+parser.add_argument('--restart-run', type=str, default='')
 args = parser.parse_args() 
 def main():
 
@@ -57,113 +65,133 @@ def main():
     comet_ml.init(project_name="rl_training")
     experiment = comet_ml.Experiment(api_key="No20MKxPKu7vWLOUQCFBRO8mo")
 
-    # Set up the environment
-    observation_type = args.observation_type
-    algorithm_name = args.algorithm_name  
-    algorithm_config_file = args.algorithm_config_file
-
-    chosen_policy = "MlpPolicy" if observation_type == 'box_dense' else "MultiInputPolicy"
-    feature_extractor = ImageDictExtractor if observation_type == 'image' or observation_type == 'topopt_game' else CustomBoxDense
- 
-
-    # Set up multiprocessing
+    # Set number of CPUs to use automatically
     num_cpu = multiprocessing.cpu_count()
-    train_env = make_vec_env(lambda: sogym(mode='train', observation_type=observation_type, vol_constraint_type='hard', resolution=50, check_connectivity=True), n_envs=num_cpu, vec_env_cls=SubprocVecEnv)
-    eval_env = make_vec_env(lambda: sogym(mode='test', observation_type=observation_type, vol_constraint_type='hard', resolution=50, check_connectivity=False), n_envs=1, vec_env_cls=SubprocVecEnv)
+    print(f"Using {num_cpu} CPUs!")
 
+    algorithm_name = args.algorithm_name  # or "TD3"
 
-    # Set up the model
-    with open(algorithm_config_file, "r") as file:
+    # Load the YAML file and extract parameters
+    with open(args.algorithm_config_file, "r") as file:
         config = yaml.safe_load(file)
 
-    algorithm_params = config[algorithm_name]
+    common_params = config.get("common", {})
+    algorithm_params = config.get(algorithm_name, {})
+
+    # Define other parameters
+    params = {
+        'observation_type': args.observation_type,
+        'vol_constraint_type': args.vol_constraint_type,
+        'use_std_strain': args.use_std_strain,
+        'check_connectivity': True,
+        'resolution': 50
+    }
+
+    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+    train_critic_only = False  # if True, we freeze everything except the critic
+    pretrained_run = None
+    restart_run = args.restart_run  # or "PPO_20240516_093938"
+
+    log_name = restart_run if restart_run else f"{algorithm_name}_{current_datetime}"
+
+    experiment.set_name(log_name)
+    # Create directory and config file if needed
+    log_dir = f'./runs/{log_name}'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+        # Combine all parameters for saving to the config file
+        save_params = {**algorithm_params, **params, 'algorithm_name': algorithm_name, **common_params}
+        with open(f'{log_dir}/config.yaml', 'w') as file:
+            yaml.dump(save_params, file)
+
+    # Create training and evaluation environments
+    train_env = sogym(mode='train', **params)
+    env = make_vec_env(lambda: train_env, n_envs=num_cpu, vec_env_cls=SubprocVecEnv)
+    env = VecCheckNan(env, raise_exception=True)
+
+    eval_env = sogym(mode='test', **params)
+    eval_env = make_vec_env(lambda: eval_env, n_envs=1, vec_env_cls=SubprocVecEnv)
+        # The noise objects for TD3
+    n_actions = env.action_space.shape[-1]
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.5 * np.ones(n_actions))
+
+    chosen_policy = "MlpPolicy" if args.observation_type == 'box_dense' else "MultiInputPolicy"
+    feature_extractor = ImageDictExtractor if args.observation_type == 'image' or args.observation_type=="topopt_game" else CustomBoxDense
+
 
     policy_kwargs = dict(
         features_extractor_class=feature_extractor,
-        net_arch=config['common']['net_arch'],
-        share_features_extractor=False
+        net_arch = config['common']['net_arch'],
+        share_features_extractor = False,
     )
 
     # Create the model based on the algorithm name and parameters
     if algorithm_name == "SAC":
-        if args.resume:
-            model = SAC.load(args.resume_path,env=train_env)
-            if args.replay_buffer_path:
-                model.load_replay_buffer(args.replay_buffer_path)
-        else:
-            model = SAC(env=train_env,
-                        policy = chosen_policy, 
-                        policy_kwargs=policy_kwargs,
-                        device=device, 
-                        **algorithm_params)
+        model = SAC(env=env,
+                    policy = chosen_policy, 
+                    policy_kwargs=policy_kwargs,
+                    #action_noise = action_noise,
+                    ent_coef = 0.0,
+                    device=device, 
+                    **algorithm_params)
 
     elif algorithm_name == "PPO":
-        if args.resume:
-            model = PPO.load(args.resume_path,env=train_env)
-        else:
-            model = PPO(env=train_env, 
-                        policy = chosen_policy, 
-                        policy_kwargs=policy_kwargs,
-                        device = device, 
-                        **algorithm_params)
+        model = PPO(env=env, 
+                    policy = chosen_policy, 
+                    policy_kwargs=policy_kwargs,
+                    n_steps= 64*386 // num_cpu//100,
+                    batch_size= 16384//4,
+                    tensorboard_log  ='./runs/{}'.format(log_name),
+                    device = device, 
+                    **algorithm_params)
 
     elif algorithm_name == "TD3":
-        if args.resume:
-            model = TD3.load(args.resume_path,env=train_env)
-            if args.replay_buffer_path:
-                model.load_replay_buffer(args.replay_buffer_path)
-        else:
-            # Create the action noise object
-            n_actions = train_env.action_space.shape[-1]
-            action_noise_params = algorithm_params.pop("action_noise")
-            action_noise = NormalActionNoise(mean=action_noise_params["mean"] * np.ones(n_actions),
-                                            sigma=action_noise_params["sigma"] * np.ones(n_actions))
-            model = TD3(env=train_env,
-                        policy =chosen_policy, 
-                        policy_kwargs=policy_kwargs,
-                        action_noise=action_noise,
-                        device=device, 
-                        **algorithm_params)
-        
-    # Set up callbacks
-    current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
-    tb_log_name = f"{algorithm_name}_{current_datetime}"
+        # Create the action noise object
+        n_actions = env.action_space.shape[-1]
+        action_noise_params = algorithm_params.pop("action_noise")
+        action_noise = NormalActionNoise(mean=action_noise_params["mean"] * np.ones(n_actions),
+                                        sigma=action_noise_params["sigma"] * np.ones(n_actions))
+        model = TD3(env=env,
+                    policy =chosen_policy, 
+                    policy_kwargs=policy_kwargs,
+                    action_noise=action_noise,
+                    device=device, 
+                    **algorithm_params)
 
+    if pretrained_run:
+        model.set_parameters("./runs/{}/checkpoints/{}".format(log_name,pretrained_run))
+
+    if restart_run:
+        model = model.load("./runs/{}/checkpoints/best_model.zip".format(log_name),env=env)
+                    
+    # Save a checkpoint every 1000 steps
     checkpoint_callback = CheckpointCallback(
-        save_freq=500000 // num_cpu,
-        save_path="./checkpoints/",
-        name_prefix=tb_log_name,
-        save_replay_buffer=True,
-        save_vecnormalize=True,
+    save_freq=250_000//num_cpu,
+    save_path="./runs/{}/checkpoints/".format(log_name),
+    name_prefix=log_name,
+    save_replay_buffer=True,
+    save_vecnormalize=True,
     )
-    early_stopping = StopTrainingOnNoModelImprovement(max_no_improvement_evals=100, min_evals=100, verbose=1)
 
     eval_callback = EvalCallback(eval_env,
-                                 log_path='tb_logs',
-                                 eval_freq=5000 // num_cpu,
-                                 deterministic=True,
-                                 n_eval_episodes=10,
-                                 render=False,
-                                 best_model_save_path='./checkpoints',
-                                 #callback_after_eval=early_stopping,
-                                 verbose=0)
-
+                                log_path='./runs/{}/'.format(log_name), 
+                                eval_freq=10_000//num_cpu,
+                                deterministic=True,
+                                n_eval_episodes=10,
+                                render=False,
+                                best_model_save_path='./runs/{}/checkpoints/'.format(log_name),
+                                verbose=0)
 
     callback_list = CallbackList([eval_callback,
-                                  checkpoint_callback,
-                                  MaxRewardCallback(verbose=1),
-                                  GradientClippingCallback(clip_value=0.5, verbose=1),
-                                  GradientNormCallback(verbose=1),
-                                  FigureRecorderCallback(check_freq=5000 // num_cpu, eval_env=eval_env),
-                                  ])
+                            checkpoint_callback,
+                            MaxRewardCallback(verbose=1),
+                            GradientClippingCallback(clip_value=1.0, verbose=1),
+                            GradientNormCallback(verbose=1),
+                            FigureRecorderCallback(eval_env=eval_env, check_freq=10_000//num_cpu, figure_size=(8, 6))
+                            ])
     
-    if args.training_phase =='critic' and not args.resume:
-        model.set_parameters(args.imitation_model_path)
-    if args.training_phase =='actor-critic' and not args.resume:
-        model.set_parameters(args.critic_model_path)
 
-
-    if args.training_phase =='critic':
+    if train_critic_only:
         #Freeze everything:
         for name, param in model.policy.named_parameters():
             if param.requires_grad:
@@ -178,20 +206,6 @@ def main():
             for param in model.policy.critic_target.parameters():
                 if param.requires_grad==False:
                     param.requires_grad=True
-            
-            #Reset critic networks:
-            if hasattr(model.policy.critic_target, 'reset_parameters'):
-                print(' resetting')
-                model.policy.critic_target.reset_parameters()
-            else:
-                model.policy.critic_target.apply(init_weights)
-
-                
-            if hasattr(model.policy.critic, 'reset_parameters'):
-                print('resetting')
-                model.policy.critic_target.reset_parameters() 
-            else:
-                model.policy.critic.apply(init_weights)
 
 
         if algorithm_name == 'PPO':
@@ -202,27 +216,17 @@ def main():
             for param in model.policy.value_net.parameters():
                 if param.requires_grad==False:
                     param.requires_grad=True
-            
+    
+    model.learn(25_000_000,
+            callback=callback_list, 
+            tb_log_name=log_name,
+            reset_num_timesteps=not restart_run
+            )
 
-            if hasattr(model.policy.value_net, 'reset_parameters'):
-                print(' resetting')
-                model.policy.value_net.reset_parameters()
-            else:
-                model.policy.value_net.apply(init_weights)
-                
-            if hasattr(model.policy.mlp_extractor.value_net, 'reset_parameters'):
-                print(' resetting')
-                model.policy.mlp_extractor.value_net.reset_parameters() 
-            else:
-                model.policy.mlp_extractor.value_net.apply(init_weights)
-
-    # Train the model
-    model.learn(100_000_000,
-                callback=callback_list,
-                tb_log_name=tb_log_name)
-
-    # Save the model
-    model.save('checkpoints/{}_final'.format(tb_log_name))
+    # save the model:
+    model.save('./runs/{}/checkpoints/final_model')
+    if algorithm_name != 'PPO':
+        model.save_replay_buffer("./runs/{}/checkpoints/final_buffer")
 
 if __name__ == "__main__":
     main()
