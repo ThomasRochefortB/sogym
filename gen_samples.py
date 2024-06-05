c97f9ce122c4e6c352b0f1f412a873dedc9bff46
Thomas Rochefort-Beaudoin <47933584+ThomasRochefortB@users.noreply.github.com>
2024-06-05 19:47:32 -0400

    Delete gen_samples.py

--- gen_samples.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['OPENBLAS_NUM_THREADS'] = '1'       #Disactivate multiprocessing for numpy
-from sogym.expert_generation import generate_dataset
-
-generate_dataset(dataset_folder = "./dataset/topologies/cortex_may13",num_threads = 24, num_samples=10000)
